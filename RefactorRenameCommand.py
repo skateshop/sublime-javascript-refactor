f124272d7f3332f5025021ec335d31b27c59d2f8
Tyler Cook <Tyler.Cook@theice.com>
2016-08-17 11:48:29 -0400

    Added command pallet and side bar refactor command options

--- RefactorRenameCommand.py
+++ RefactorRenameCommand.py
@@ -1,6 +1,7 @@
-import sublime
+Hello, World!import sublime
 import sublime_plugin
 
 class RefactorRenameCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
+  
+  def run(self, edit, **args):
     self.view.insert(edit, 0, "Hello, World!")
